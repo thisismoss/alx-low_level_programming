39afc1fc57ed05aae0c3a24feac37dc2881a761d
root <root@DESKTOP-IGJ2SAC.localdomain>
2023-03-17 11:34:54 +0100

    Programs, Done!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints with put funtion
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using Sizeof funtion to print the size of various types
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a Lng int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a Long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
