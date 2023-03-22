daa864ce167c22572cef23a35b96dd3aa6f4b63b
root <root@DESKTOP-IGJ2SAC.localdomain>
2023-03-22 00:48:50 +0000

    Functions, DONE!

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-
 /**
  * print_alphabet - print all alphabet in lowercase 10 times
  **/
 
 void print_alphabet(void)
 {
-	int letter, i;
+	char letter;
+	int i;
 
 	for (i = 1; i <= 10; i++)
 	{
