667f1cf8a688b606af18a78afeffa7d89a9e3bb1
root <root@DESKTOP-IGJ2SAC.localdomain>
2023-03-23 15:15:41 +0000

    Functions, DONE!

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,10 @@ void print_times_table(int n)
 	
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
-			for (mult = 1; mult <= n; ++mult)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -29,7 +29,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
-					_putchar((prod / 100) % 10 + 48);
+					_putchar((prod / 10) % 10 + 48);
 				} 
 				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
